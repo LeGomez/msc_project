8a7a6e2c38da1db4294c41eaaef201dd4d28a814
LeGomez <lmsilvagomez@gmail.com>
2022-07-23 19:06:56 +0100

    feat: refactor sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,15 +1,9 @@
-from textblob import TextBlob
 import pandas as pd
 import os
 import boto3
 from generate_data.helpers import upload_file
 from io import BytesIO
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-
-def percentage(part,whole):
- return format(100 * float(part)/float(whole), '.1f')
 
 bucket = 'luis-project-twitter-data'
 file = 'tweets.csv'
@@ -29,7 +23,6 @@ tweets['created_at'] = pd.to_datetime(tweets['created_at']).dt.date
 tweets['created_at'] = tweets['created_at'].astype("string")
 
 
-
 positive = 0
 negative = 0
 neutral = 0
@@ -42,13 +35,11 @@ daily_results = {}
 
 for index, tweet in tweets.iterrows():
     tweet_list.append([tweet['text'], tweet['created_at']])
-    analysis = TextBlob(tweet['text'])
     score = SentimentIntensityAnalyzer().polarity_scores(tweet['text'])
     neg = score['neg']
     neu = score['neu']
     pos = score['pos']
     comp = score['compound']
-    polarity += analysis.sentiment.polarity
 
     if neg > pos:
         if tweet['created_at'] in daily_results:
@@ -71,10 +62,7 @@ for index, tweet in tweets.iterrows():
             daily_results[tweet['created_at']] = [0, 0, 1]
         neutral += 1
 
-# positive = percentage(positive, tweet_num)
-# negative = percentage(negative, tweet_num)
-# neutral = percentage(neutral, tweet_num)
-# polarity = percentage(polarity, tweet_num)
+
 print(f'Positive={positive}, negative={negative}, neutral={neutral}')
 print(len(tweets))
 
